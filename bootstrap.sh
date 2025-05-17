7936be9a2e9d7fc3585194b1985704cf37b80129
Carlos Ariel de la Torre <cwebtecnico@gmail.com>
2025-05-17 14:28:27 +0200

    chore: we have added new dependencies

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,12 +8,12 @@ if [ ! -f ~/.ssh/id_rsa ]; then
     ssh-keygen -b 4096 -t rsa -f ~/.ssh/id_rsa
     cat ~/.ssh/id_rsa.pub | tee -a ~/.ssh/authorized_keys > /dev/null
 fi
-# if [ ! -f ~/.ssh/id_ed25519 ]; then
-#     ssh-keygen -t ed25519 -a 100 -b 4096
-#     cat ~/.ssh/id_ed25519.pub | tee -a ~/.ssh/authorized_keys > /dev/null
-# fi
+#if [ ! -f ~/.ssh/id_ed25519 ]; then
+#    ssh-keygen -t ed25519 -a 100 -b 4096
+#    cat ~/.ssh/id_ed25519.pub | tee -a ~/.ssh/authorized_keys > /dev/null
+#fi
 if [ "$OS" = "Ubuntu" ]; then
-    sudo apt update && sudo apt -y install nano git make python3-virtualenv ansible openssh-server firewalld python3-firewall python3.10-venv
+    sudo apt update && sudo apt -y install nano git make python3-virtualenv ansible openssh-server firewalld python3-firewall python3-venv python3-debian
     sudo update-alternatives --set editor $(which nano)
     echo -e "PubkeyAuthentication yes\nX11Forwarding no" | sudo tee /etc/ssh/sshd_config.d/90_permit_ssh_key_authentication.conf > /dev/null
     sudo systemctl reload ssh && sudo systemctl enable ssh && sudo systemctl stop ssh && sudo systemctl start ssh
